6d562c104093123d24b549834986dead66a0928d
Luiz Guilherme Reis <luixguilherme@hotmail.com>
2023-08-07 19:50:06 -0300

    :bricks: ci: Add project pages

---
+++ src/routes/index.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "../layouts/Dashboard";
+import Home from "../pages/Home";
+import Salas from "../pages/Salas";
+import Responsaveis from "../pages/Responsaveis";
+import Eventos from "../pages/Eventos";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import { useContext, useEffect } from "react";
+import { LoginContext } from "../App";
+
+export const Ways = () => {
+
+    const { logado } = useContext(LoginContext);
+
+    useEffect(() => {
+        console.log(logado);
+    }, [logado]);
+
+    return(
+        <>
+            <BrowserRouter>
+                <Routes>
+                    {
+                        logado ? (
+                            <Route path="/" element={<Dashboard />}>
+                                <Route index element={<Home />} />
+                                <Route path="/salas" element={<Salas />} />
+                                <Route path="/responsaveis" element={<Responsaveis />} />
+                                <Route path="/eventos" element={<Eventos />} />
+                            </Route>
+                        ) : (
+                            <Route path="/" element={<Login />} />
+                        )
+                    }
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
